bb95c3f8b6a3fc824ee8b0b0aca4c0a82a9eba69
S.Ishigaki <sagamisen315@yahoo.co.jp>
2013-10-29 18:34:19 +0900

    Ver 1.6
    README.md
    src/bookmarklet.html
    src/JS.stringify.js
    src/JS.stringify.min.js

--- src/JS.stringify.js
+++ src/JS.stringify.js
@@ -1,12 +1,12 @@
 /*
- * JS.stringify v1.5
+ * JS.stringify v1.6
  * http://junk-box.appspot.com/bookmarklet/JS.stringify/index.html
  *
  * Copyright (C) 2013 S.Ishigaki
  * Licensed under the MIT license
  * http://www.opensource.org/licenses/mit-license.php
  *
- * Date: 2013-8-20
+ * Date: 2013-10-29
  */
 (function() {
 
@@ -33,6 +33,10 @@ var
 		return s;
 	},
 
+	viewFont = "normal 12px/1 sans-serif",
+
+	viewTag = "<div style=\"margin: 0px;\"><span style=\"font: " + viewFont + ";\">",
+
 	count,
 
 	stringEscape = function(s) {
@@ -48,7 +52,7 @@ var
 		var ot = typeof(obj);
 		if (ot != "object" && ot != "function") return ot == "string" ? "\"" + stringEscape(obj) + "\"" : obj;
 		if (depth == indent) return "{...}";
-		var t, v, json = [], arr = (obj && obj.constructor == Array);
+		var t, v, json = [], isArray = (obj && obj.constructor == Array);
 		for (var n in obj) {
 			v = obj[n];
 			t = typeof(v);
@@ -61,11 +65,12 @@ var
 				else if (t == "string") v = "\"" + stringEscape(v) + "\"";
 				else if (t == "object" &&  v != null) v = arguments.callee(v, depth, indent + 1, fn);
 				else v = String(v);
-				json.push((arr ? "" : "\"" + stringEscape(n) + "\": ") + v);
+				json.push((isArray ? "" : "\"" + stringEscape(n) + "\": ") + v);
 				if (count > 1000) throw new Error("too big object.");
 			}
 		}
-		return (arr ? "[" : "{") + "</span></div><div><span>" + toIndent(indent + 1) + json.join(",</span></div><div><span>" + toIndent(indent + 1)) + "</span></div><div><span>" + toIndent(indent) + (arr ? "]" : "}");
+		var s = "</span></div>" + viewTag;
+		return (isArray ? "[" : "{") + s + toIndent(indent + 1) + json.join("," + s + toIndent(indent + 1)) + s + toIndent(indent) + (isArray ? "]" : "}");
 	},
 
 	stringify = function() {
@@ -80,7 +85,7 @@ var
 				} catch(_ex) {}
 				return o == undefined ? eval(input.value) : o;
 			})();
-			return "<div><span>" + _stringify(obj, isNaN(depth.innerHTML) ? 1000 : Number(depth.innerHTML), 0, fn.style.color == "rgb(82, 82, 82)") + "</span></div>";
+			return viewTag + _stringify(obj, isNaN(depth.innerHTML) ? 1000 : Number(depth.innerHTML), 0, fn.style.color == "rgb(82, 82, 82)") + "</span></div>";
 		} catch (ex) {
 			return ex.message;
 		}
@@ -92,7 +97,7 @@ var
 		select.style.cssText = toCssText([
 			"color: #609",
 			"background-color: #fffbf0",
-			"font: normal 12px/1 sans-serif",
+			"font: " + viewFont,
 			"border-width: 0px",
 			"cursor: pointer"
 		]);
@@ -110,7 +115,7 @@ var
 			"margin-left: 8px",
 			"color: #609",
 			"background-color: #fffbf0",
-			"font: normal 12px/1 sans-serif",
+			"font: " + viewFont,
 			"border-width: 0px",
 			"cursor: pointer"
 		]);
@@ -121,6 +126,7 @@ var
 
 		var action = document.createElement("div");
 		action.style.cssText = toCssText([
+			"margin: 0px",
 			"margin-top: 5px",
 			"text-align: right"
 		]);
@@ -136,9 +142,12 @@ var
 			"position: fixed",
 			"top: 10%",
 			"left: 10%",
+			"margin: 0px",
+			"z-index: 2147483647",
+			"width: auto",
+			"height: auto",
 			"max-width: 80%",
 			"max-height: 80%",
-			"z-index: 2147483647",
 			"text-align: left",
 			"border: solid 1px #666",
 			"background-color: #fffbf0",
@@ -150,6 +159,7 @@ var
 
 		var rmPx = function(len) {return Number(len.substring(0, len.length - 2));};
 		str.style.cssText = toCssText([
+			"margin: 0px",
 			"margin-top: 10px",
 			"overflow: auto",
 			"max-width: " + (Math.floor(window.innerWidth * 0.8) - rmPx(view.style.paddingLeft) - rmPx(view.style.paddingRight)) + "px",
@@ -202,8 +212,10 @@ var
 		watch.title = "watch";
 		watch.innerHTML = "=";
 		watch.style.cssText = toCssText([
+			"display: inline-block",
 			"position: relative",
 			"top: 2px",
+			"margin: 0px",
 			"font: bold 19px/1.0 Comic Sans MS",
 			"border: 1px solid #aaaaab",
 			"border-right-width: 0px",
@@ -218,8 +230,7 @@ var
 			"color: rgb(82, 82, 82)",
 			"text-shadow: rgb(255, 255, 255) 1px 1px 0px",
 			"outline: none",
-			"cursor: pointer",
-			"display: inline-block"
+			"cursor: pointer"
 		]);
 		watch.onclick = function() {
 			show();
@@ -234,6 +245,7 @@ var
 		depth.style.cssText = toCssText([
 			"position: relative",
 			"top: 2px",
+			"margin: 0px",
 			"font: bold 18px/1.0 Comic Sans MS",
 			"border: 1px solid #aaaaab",
 			"border-right-width: 0px",
@@ -257,8 +269,10 @@ var
 			else if (this.innerHTML == "5") {
 				this.innerHTML = "&infin;";
 				this.style.cssText = toCssText([
+					"display: inline-block",
 					"position: relative",
 					"top: 2px",
+					"margin: 0px",
 					"font: bold 18px/1.0 Comic Sans MS",
 					"border: 1px solid #aaaaab",
 					"border-right-width: 0px",
@@ -273,14 +287,15 @@ var
 					"color: rgb(82, 82, 82)",
 					"text-shadow: rgb(255, 255, 255) 1px 1px 0px",
 					"outline: none",
-					"cursor: pointer",
-					"display: inline-block"
+					"cursor: pointer"
 				]);
 			} else {
 				this.innerHTML = "1";
 				this.style.cssText = toCssText([
+					"display: inline-block",
 					"position: relative",
 					"top: 1px",
+					"margin: 0px",
 					"font: bold 14px/1.0 Comic Sans MS",
 					"border: 1px solid #aaaaab",
 					"border-right-width: 0px",
@@ -295,10 +310,10 @@ var
 					"color: rgb(82, 82, 82)",
 					"text-shadow: rgb(255, 255, 255) 1px 1px 0px",
 					"outline: none",
-					"cursor: pointer",
-					"display: inline-block"
+					"cursor: pointer"
 				]);
 			}
+			show();
 		};
 		return depth;
 	})(),
@@ -308,8 +323,10 @@ var
 		fn.title = "include function";
 		fn.innerHTML = "fn";
 		fn.style.cssText = toCssText([
+			"display: inline-block",
 			"position: relative",
 			"top: 1px",
+			"margin: 0px",
 			"font: bold 15px/1.0 Arial",
 			"border: 1px solid #aaaaab",
 			"border-right-width: 0px",
@@ -324,15 +341,16 @@ var
 			"color: rgb(82, 82, 82)",
 			"text-shadow: rgb(255, 255, 255) 1px 1px 0px",
 			"outline: none",
-			"cursor: pointer",
-			"display: inline-block",
+			"cursor: pointer"
 		]);
 		fn.onclick = function() {
 			var status = this.style.color == "rgb(82, 82, 82)";
 			this.title = status ? "exclude function" : "include function";
 			this.style.cssText = toCssText([
+				"display: inline-block",
 				"position: relative",
 				"top: 1px",
+				"margin: 0px",
 				"font: bold 15px/1.0 Arial",
 				"border: 1px solid #aaaaab",
 				"border-right-width: 0px",
@@ -347,9 +365,9 @@ var
 				"color: " + (status ? "rgb(66, 129, 235)" : "rgb(82, 82, 82)"),
 				"text-shadow: " + (status ? "rgb(129, 169, 237) 0px 0px 1px" : "rgb(255, 255, 255) 1px 1px 0px"),
 				"outline: none",
-				"cursor: pointer",
-				"display: inline-block"
+				"cursor: pointer"
 			]);
+			show();
 		};
 		return fn;
 	})(),
@@ -359,8 +377,10 @@ var
 		close.title = "close";
 		close.innerHTML = "&times;";
 		close.style.cssText = toCssText([
+			"display: inline-block",
 			"position: relative",
 			"top: 2px",
+			"margin: 0px",
 			"font: bold 17px/1.0 Impact",
 			"border: 1px solid #aaaaab",
 			"border-top-color: #969696",
@@ -372,7 +392,6 @@ var
 			"text-decoration: none",
 			"background-color: #eeeeef",
 			"color: #525252",
-			"display: inline-block",
 			"text-shadow: 1px 1px 0px #ffffff",
 			"outline: none",
 			"cursor: pointer"
@@ -390,7 +409,10 @@ var
 			"position: fixed",
 			"top: 0px",
 			"right: 3px",
+			"margin: 0px",
 			"z-index: 2147483647",
+			"width: auto",
+			"height: auto",
 			"padding: 3px",
 			"padding-top: 1px",
 			"white-space: nowrap",
